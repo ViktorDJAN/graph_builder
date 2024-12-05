7279e641b8c8eb6324a10a397776c1641879d205
Viktor Kashtanov <pistoletviktor@gmail.com>
2024-12-05 09:58:07 +0300

    checkboxes are processed

--- appVisual/script.js
+++ appVisual/script.js
@@ -1,5 +1,3 @@
-let arrayWithResponses = [];
-
 // parameters
 
 let startStop;
@@ -17,6 +15,20 @@ let contactorStatus;
 let protocol;
 
 
+let checkBoxEv_I = document.getElementById("cb_ev_i");
+let checkBoxEv_U = document.getElementById("cb_ev_u");
+let checkBoxEVSE_I = document.getElementById("cb_evse_i");
+
+let checkBoxEVSE_U = document.getElementById("cb_evse_u");
+let checkBoxCP_U = document.getElementById("cb_cp_u");
+let checkBoxCPFreq = document.getElementById("cb_cp_freq");
+let checkBoxCPDutyCycle = document.getElementById("cb_cp_duty_cycle");
+let checkBoxDtMsg = document.getElementById("cb_dt_msg");
+let checkBoxStage = document.getElementById("cb_stage");
+
+
+
+
 function getDataXMLHttpRequest() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://127.0.0.1:3060/get_data', false);
@@ -29,170 +41,80 @@ function getDataXMLHttpRequest() {
     // console.log(param)
     // console.log(param.sentData.evse_U)
 
-    const jsonObj = new Function('return ' + jsonString)();
+    const jsonObj = new Function('return ' + jsonString)(); // string to json obj
     // console.log(jsonObj)
-   // console.log(jsonObj["sentData"]["ev_u"])
+    // console.log(jsonObj["sentData"]["ev_u"])
 
 
     try {
-        buildGr(); // builds horizontal line
-        if (jsonObj["sentData"]!== null || jsonObj["sentData"] !== undefined) {
-            // building graphs with outgoing params  ___________________________
-            console.log(jsonObj["sentData"])
-            buildGraph("evse_U", dataSetEVSE_U, jsonObj["sentData"]);
-            buildGraph("evse_I", dataSetEVSE_I, jsonObj["sentData"]);
-            buildGraph("cp_U", dataSetCP_U, jsonObj["sentData"]);
-            buildGraph("cp_Freq", dataSetCP_Freq, jsonObj["sentData"]);
-            buildGraph("cp_DutyCicle", dataSetCP_DutyCycle, jsonObj["sentData"]);
-            buildGraph("dt_message", dataSetDt_message, jsonObj["sentData"]);
-            buildGraph("stage", dataSetStage, jsonObj["sentData"]);
+        // builds horizontal line
+        if (jsonObj["sentData"] !== null || jsonObj["sentData"] !== undefined) {
+            // building graphs with outgoing params  __________________________
+            buildGraphAndCB("evse_U", dataSetEVSE_U, jsonObj["sentData"],checkBoxEVSE_U);
+            buildGraphAndCB("evse_I", dataSetEVSE_I, jsonObj["sentData"],checkBoxEVSE_I);
+            buildGraphAndCB("cp_U", dataSetCP_U, jsonObj["sentData"],checkBoxCP_U);
+            buildGraphAndCB("cp_Freq", dataSetCP_Freq, jsonObj["sentData"],checkBoxCPFreq);
+            buildGraphAndCB("cp_DutyCicle", dataSetCP_DutyCycle, jsonObj["sentData"],checkBoxCPDutyCycle);
+            buildGraphAndCB("dt_message", dataSetDt_message, jsonObj["sentData"],checkBoxDtMsg);
+            buildGraphAndCB("stage", dataSetStage, jsonObj["sentData"],checkBoxStage);
         }
-        if (jsonObj["receivedData"]!== null || jsonObj["receivedData"] !== undefined) {
-            buildGraph("ev_u", dataSetEV_U, jsonObj["receivedData"]);
-            buildGraph("ev_i", dataSetEV_I, jsonObj["receivedData"]);
-
+        if (jsonObj["receivedData"] !== null || jsonObj["receivedData"] !== undefined) {
+            buildGraphAndCB("ev_u", dataSetEV_U, jsonObj["receivedData"],checkBoxEv_U);
+            buildGraphAndCB("ev_i", dataSetEV_I, jsonObj["receivedData"],checkBoxEv_I);
         }
-
-        // if (jsonObj["receivedData"] !== null || jsonObj["receivedData"] !== undefined) {
-        //     // console.log("EVERYTHING iS OK" + Object.keys(jsonObj[1]))
-        //     buildGraph("evse_U", dataSetEVSE_U, jsonObj["receivedData"]);
-        //     buildGraph("evse_I", dataSetEVSE_I, jsonObj["receivedData"]);
-        //     buildGraph("cp_U", dataSetCP_U, jsonObj["receivedData"]);
-        //     buildGraph("cp_Freq", dataSetCP_Freq, jsonObj["receivedData"]);
-        //     buildGraph("cp_DutyCicle", dataSetCP_DutyCycle, jsonObj["receivedData"]);
-        //     buildGraph("dt_message", dataSetDt_message, jsonObj["receivedData"]);
-        //     buildGraph("stage", dataSetStage, jsonObj["receivedData"]);
-        //
-        // }
-
-
-
-
-
-
-
-
-
-
-
-
-            // console.log(ev_u + '__EV_U_ PARAM')
-            // ev_i = jsonObj[0]["ev_i"];   // build graph
-            // ev_maxU = jsonObj[0]["ev_maxU"];
-            // ev_maxI = jsonObj[0]["ev_maxI"];
-            // ev_maxP = jsonObj[0]["ev_maxP"];
-            // timeCharge = jsonObj[0]["timeCharge"];
-            // cp_on = jsonObj[0]["cp_on"];
-            // err_code = jsonObj[0]["err_code"];
-            // ready = jsonObj[0]["ready"];
-            // soc = jsonObj[0]["soc"];
-            // contactorStatus = jsonObj[0]["contactorStatus"];
-            // protocol = jsonObj[0]["protocol"];
+        if (jsonObj["receivedData"] !== null || jsonObj["sentData"] !== null) {
+            buildAxisX();
+        }
 
     } catch (e) {
         console.log('Empty object')
     }
 
-
-    // // getting incoming parameters ___________________________
-    // let startStop = jsonObj[0]["startStop"];
-    // let ev_u = jsonObj[0]["ev_u"];   // build graph
-    // let ev_i = jsonObj[0]["ev_i"];   // build graph
-    // let ev_maxU = jsonObj[0]["ev_maxU"];
-    // let ev_maxI = jsonObj[0]["ev_maxI"];
-    // let ev_maxP = jsonObj[0]["ev_maxP"];
-    // let timeCharge = jsonObj[0]["timeCharge"];
-    // let cp_on = jsonObj[0]["cp_on"];
-    // let err_code = jsonObj[0]["err_code"];
-    // let ready = jsonObj[0]["ready"];
-    // let soc = jsonObj[0]["soc"];
-    // let contactorStatus = jsonObj[0]["contactorStatus"];
-    // let protocol = jsonObj[0]["protocol"];
-
-    // // getting outgoing parameters
-    // let evse_U = jsonObj[0]["evse_U"];    // build graph
-    // let evse_I = jsonObj[0]["evse_I"];    // build graph
-    // let evse_maxU = jsonObj[0]["evse_maxU"];
-    // let evse_maxI = jsonObj[0]["evse_maxI"];
-    // let evse_maxP = jsonObj[0]["evse_maxP"];
-    // let stage = jsonObj[0]["stage"];     // build graph
-    // let contactorRequest = jsonObj[0]["contactorRequest"];
-    // let cp_U = jsonObj[0]["cp_U"];                  // build graph
-    // let cp_DutyCicle = jsonObj[0]["cp_DutyCicle"];  // build graph
-    // let cp_Freq = jsonObj[0]["cp_Freq"];        // build graph
-    // let dt_message = jsonObj[0]["dt_message"];  // build graph
-
 }
+
+
 
 setInterval(getDataXMLHttpRequest, 1000);
 
 
-async function parseResponse(paramIndex) {
-    if (arrayWithResponses.length === 0) {
-        setTimeout(100)
-        // console.log('THERE IS NULL')
-    } else {
-        let paramValue = arrayWithResponses.shift();
-        // console.log(paramValue + "IT RETURNED" + arrayWithResponses.length)
-        let arrayWithParams = paramValue.split("__");
-        return arrayWithParams[paramIndex];
 
-    }
-
-
-}
-
-
+// time delay
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+// await sleep(500);
 let counter = 0;
 
+
 // GRAPH BUILDERS
+async function buildGraphAndCB(paramName, dataSet, array,checkBoxState) {
+    if (checkBoxState.checked===true) {
+    buildGraph(paramName, dataSet, array)
+    }
+}
 
 async function buildGraph(paramName, dataSet, array) {
     let param;
-    // console.log(paramName)
-    // console.log(array)
-    if(array!==null && array!==undefined){
+    if (array !== null && array !== undefined) {
         param = array[paramName];
-
         let number = await transform(param);
         if (!isNaN(number)) {
             console.log(number + "N")
             dataSet.data.push(number);
+        }
+        chart1.update();
     }
-    // if (array[paramName] !== null && array[paramName] !== undefined) {
-    //     param = array[paramName];
-    // }
-    // let number = await transform(param);
-    // if (!isNaN(number)) {
-    //     dataSet.data.push(number);
-    //
-  }
-
-
 }
 
-async function buildGr() {
-    // Задержка в 0.5 секунды перед выводом сообщения
-
-
+async function buildAxisX() {
     chart1.data.labels.push(counter);
     counter++;
-    chart1.update();
-
-    // await sleep(500);
 }
 
 
 async function transform(data) {
     let a = Number(data);
     if (!isNaN(a) || a !== undefined)
-        return a;
-
+        return a
 }
-
-
-// setInterval(buildGraph, 500);
 
 
 let dataSetEV_U = createDataSet("EV_U", "#a8acfd", "#a8acfd");
@@ -207,17 +129,6 @@ let dataSetStage = createDataSet("Stage", "#ff0000", "#ff0000");
 
 
 const ctx = document.getElementById('myChart');
-
-
-let dataSetA = {
-    label: 'JavaScript Developer',
-    lineTension: 0,
-    backgroundColor: 'blue',
-    borderColor: 'blue',
-    data: [],
-    borderWidth: 1
-};
-
 
 function createDataSet(label, backgroundColor, borderColor) {
     return {
